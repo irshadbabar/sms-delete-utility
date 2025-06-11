f29fc7d85fe182b1fef64644589c597883684c3b
irshadbabar <38275048+irshadbabar@users.noreply.github.com>
2025-06-11 12:25:22 +0500

    testing push

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App1 from './src/App_Component';
 //import App from './App';
+// just comment testing for push
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App1);
